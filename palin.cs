245e908297e73b1cf24667006e3c5a48209582cf
Student <example@example.com>
2020-02-25 11:56:48 -0800

    add method to reverse array

--- palin.cs
+++ palin.cs
@@ -1,13 +1,31 @@
 using System;
 using System.Collections.Generic;
-public class Palindrome
-{
 
+static class Palindrome
+{
+  public static string ReverseString(string s)
+  {
+    char[] arr = s.ToCharArray();
+    Array.Reverse(arr);
+    return new string(arr);
+  }
 }
-public class Program 
+
+public class Program
 {
   public static void Main()
   {
-    
+    Console.WriteLine("Enter a word that could be a palindrome");
+    string userInput = Console.ReadLine();
+    if ( userInput == Palindrome.ReverseString(userInput)) 
+    {
+      Console.WriteLine("This is a Palindrome");
+    }   
+    else 
+    {
+      Console.WriteLine("This is not a Palindrome");
+    }
   }
 }
+
+
